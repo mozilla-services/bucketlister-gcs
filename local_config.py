339ac2a5b7386ddd56c7accd66f79e6958faf5a5
Chris Valaas <cvalaas@mozilla.com>
2022-10-20 13:57:33 -0700

    putting in prod memcached host for testing

--- local_config.py
+++ local_config.py
@@ -1,6 +1,6 @@
 from os import getenv
 
-MEMCACHED_HOST = getenv("MEMCACHED_HOST", "localhost")
+MEMCACHED_HOST = getenv("MEMCACHED_HOST", "172.16.17.3")
 BUCKET         = getenv("GCS_PRODUCTDELIVERY_BUCKET", "moz-fx-productdelivery-pr-38b5-productdelivery")
 
 # these paths need to end with a /
